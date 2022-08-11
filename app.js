c56fedd4ee719fda6f5cc0c7eb11a9e5cd352a29
ifavila <ifavila8@gmail.com>
2022-08-11 15:37:21 -0700

    added react front end

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const wordRouter = require('./controllers/words')
-const cors = require('cors')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const wordRouter = require("./controllers/words");
+const cors = require("cors");
 
-mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB: ', error.message)
-    })
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB: ", error.message);
+  });
 
-app.use(cors())
-app.use('/api/words', wordRouter)
+app.use(cors());
+app.use("/api/words", wordRouter);
+app.use(express.static('build'))
 
-module.exports = app
+module.exports = app;
--- controllers/words.js
+++ controllers/words.js
@@ -1,14 +1,32 @@
-const wordRouter = require('express').Router()
-const res = require('express/lib/response')
-const Word = require('../models/word')
+const wordRouter = require("express").Router();
+const Word = require("../models/word");
 
-wordRouter.get('/', (request, response) => {
-    Word.find({
-        "word": { $exists: true },
-        $expr: { $gt: [ { $strLenCP:"$word"}, 14]}
-    }).then(word => {
-        response.json(word)
-    })
-})
+wordRouter.get("/", async (request, response) => {
+  //const word = await Word.find({})
+  const word = await Word.find({});
+});
 
-module.exports = wordRouter
+wordRouter.get("/:word", async (request, response) => {
+  const word = await Word.find({
+    $expr: { $lt: [{ $strLenCP: "$word" }, request.params.word.length + 1] },
+  });
+  const inputWord = request.params.word;
+  const filtered = [];
+  word.forEach((word) => {
+    if (
+      word.word
+        .split("")
+        .every(
+          (val) =>
+            inputWord.split("").includes(val) &&
+            word.word.split("").filter((el) => el === val).length <=
+              inputWord.split("").filter((el) => el === val).length
+        )
+    ) {
+      filtered.push(word);
+    }
+  });
+
+  response.json(filtered);
+});
+module.exports = wordRouter;
